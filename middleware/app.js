ce87e2af3cad5e7411f00e9013286d72bb83f0c8
Siam Ahmed <siamahmed.only@gmail.com>
2026-02-07 14:31:43 +0600

    07-2-26

--- middleware/app.js
+++ middleware/app.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const app = express();
 
@@ -128,7 +129,7 @@ app.get("/", (req, res) => {
 
 
 // nirdisto route er jonno middleware
-app.use("/admin", (req, res, next) => {
+/* app.use("/admin", (req, res, next) => {
     console.log("Trying to logging");
 
     const isAdmin = req.query.admin === 'true';
@@ -150,7 +151,137 @@ app.get("/admin/settings", (req, res) => {
 
 app.get("/home", (req, res) => {
     res.send("Welcome to the Home Page!");
+}); */
+
+
+
+
+
+
+// ===================Router-level Middleware====================
+
+/*
+
+// create router 
+const adminRouter = express.Router();
+
+// router level middleware
+adminRouter.use((req, res, next) => {
+    console.log("Admin router a ekti request esheche...");
+
+    if(req.query.token === "12345") {
+        next();
+    } else {
+        res.status(403).send("Apnar admin access nei!");
+    }
+
 });
+
+// admin dashboard route
+adminRouter.get("/dashboard", (req, res) => {
+    res.send("Welcome to the Admin Dashboard!");
+});
+
+// admin settings route
+adminRouter.get("/settings", (req, res) => {
+    res.send("Welcome to the Admin Settings!");
+});
+
+
+app.use("/admin", adminRouter);
+
+app.get("/home", (req, res) => {
+    res.send("Welcome to the Home Page!");
+});
+
+*/
+
+
+
+/* const userRouter = express.Router();
+
+// router level middleware
+userRouter.use((req, res, next) => {
+    console.log("User router a ekti request esheche...");
+    
+    if(req.query.auth === "true") {
+        next();
+    } else {
+        res.status(404).send("User authentication failed!");
+    }
+});
+
+
+//user route 
+userRouter.get("/profile", (req, res) => {
+    res.send("Welcome to your profile page!");
+});
+
+userRouter.get("/orders", (req, res) => {
+    res.send("Welcome to your orders page!");
+});
+
+app.use("/user", userRouter);
+
+app.get("/home", (req, res) => {
+    res.send("Welcome to the Home Page!");
+});
+ */
+
+
+
+// =============Error-handling Middleware===========
+/* app.get("/", (req, res) => {
+    res.send("Welcome to the home page!");
+});
+
+// error handling middleware
+app.get("/broken", (req, res, next) => {
+    const err = new Error("Something went wrong!");
+    next(err);
+});
+
+app.get("/test", (req, res, next) => {
+    try {
+        console.log(myVariable);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    console.log("server err record kora holo", err.message);
+
+    // new msg for user
+    res.status(500).json({
+        success: false,
+        message: err.message || "Internal Server Error"
+    });
+});
+
+*/
+
+
+
+app.get("/add", (req, res, next) => {
+    const a = req.query.a;
+    const b = req.query.b;
+
+    if(!a || !b) {
+        const error = new Error("Please provide both 'a' and 'b' query parameters.");
+        next(error);
+    } else {
+        res.send(`The sum of ${a} and ${b} is ${Number(a) + Number(b)}.`);
+    }
+});
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    res.status(400).send(err.message);
+});
+
 
 
 
